ca56572da0b37a0a96998841e971a0f882c5a028
Pedrogarcia011 <pedrohenriquegracia011@gmail.com>
2022-11-12 11:31:40 -0300

    Projeto Gerador de Senhas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('ahh');
+import { nanoid } from 'nanoid'
+import "./style.css"
+
+const bottonEl = document.querySelector('button')
+const scriptEl = document.querySelector('#subscript')
+
+bottonEl.addEventListener('click', script)
+
+function script (){
+    scriptEl.innerHTML = nanoid();
+}
