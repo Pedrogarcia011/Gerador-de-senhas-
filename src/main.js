28b2bc90f8416f0dff2e370cc52e8594d5127244
Pedrogarcia011 <pedrohenriquegracia011@gmail.com>
2022-11-12 12:11:31 -0300

    att css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid'
 import "./style.css"
+import copy from 'clipboard-copy';
 
 const bottonEl = document.querySelector('button')
 const scriptEl = document.querySelector('#subscript')
@@ -9,3 +10,8 @@ bottonEl.addEventListener('click', script)
 function script (){
     scriptEl.innerHTML = nanoid();
 }
+
+scriptEl.addEventListener('click', (event) => {
+    copy(event.target.innerHTML)
+    alert('Senha copiada!')
+})
